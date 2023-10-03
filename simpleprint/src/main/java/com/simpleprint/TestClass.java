bf9fed9516fac07f39749cbfa7e2aaec502b6599
brianleomelo <85468911+brianleomelo@users.noreply.github.com>
2023-10-02 21:15:10 -0300

    Clase TestClass agregada

---
+++ simpleprint/src/main/java/com/simpleprint/TestClass.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.simpleprint;
+
+/**
+ *
+ * @author brian
+ */
+public class TestClass {
+    
+    private String hola;
+
+    public TestClass(String hola) {
+        this.hola = hola;
+    }
+    
+    
+}
